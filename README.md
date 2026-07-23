a39db09f4b11d0d50582838c3f432605215cedfc
Showtc <46968067+Showtc@users.noreply.github.com>
2026-07-23 14:20:11 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desviament - Proton Bus Mods
+
+Chromium and Gecko based browser that bypass the 10 seconds waiting to download a mod on protonbusmods.com website.
---
+++ content-script.js
@@ -0,0 +1,26 @@
+// Extract the link ID
+function extractIdFromLink(href) {
+  const regex = /\/download\/(\d+)\//;
+  const match = href.match(regex);
+  return match ? match[1] : null;
+}
+
+// Update links
+function updateLinks() {
+  const links = document.querySelectorAll('a[href^="/download/"]');
+
+  links.forEach(link => {
+    const id = extractIdFromLink(link.href);
+    if (id) {
+      link.href = `/track_download.php?id=${id}`;
+      // console.log(`Link updated : ${link.href} (ID: ${id})`);
+    }
+  });
+}
+
+// Execute when loading page
+document.addEventListener('DOMContentLoaded', updateLinks);
+
+// Observe updates
+const observer = new MutationObserver(updateLinks);
+observer.observe(document.body, { childList: true, subtree: true });
